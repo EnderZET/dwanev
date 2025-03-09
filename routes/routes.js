0f5492a6eee53fe337545bc1f7fb427f6f303727
Cethydexyl <115909230+ChronicleDevs@users.noreply.github.com>
2025-03-09 14:52:59 +0700

    Update routes.js

--- routes/routes.js
+++ routes/routes.js
@@ -798,7 +798,7 @@ router.post('/update-score-sub/:id', requireAuth, async (req, res) => {
       // Perbarui skor sesuai aksi
 
       // Update skor di database
-      await connectionSkor.query('UPDATE skor SET skor = $1 WHERE uid = $2', [cskor-parseInt(penalties[dropdown]), id]);
+      await connectionSkor.query('UPDATE skor SET skor = $1 WHERE uid = $2', [cskor-penalties[dropdown], id]);
     }
 
     // Kembali ke halaman skor dengan data yang diperbarui
